abce619e7b311c527e826562cc4b96694b7b096b
John S <john.es.saunders@gmail.com>
2026-04-30 13:32:17 -0400

    Initial Commit

---
+++ SlideExportPOC/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SlideExportPOC
+//
+//  Created by John Saunders on 4/30/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SlideExportPOC/SlideExportPOCApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideExportPOCApp.swift
+//  SlideExportPOC
+//
+//  Created by John Saunders on 4/30/26.
+//
+
+import SwiftUI
+
+@main
+struct SlideExportPOCApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
